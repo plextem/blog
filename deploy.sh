b25ab8cdbc2a432901add55e1311a8edcbdaeab3
SysCon <sys_con@outlook.com>
2018-09-04 16:07:33 +0800

    blog

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-mkdocs build --clean
+python -m mkdocs build --clean
 echo "site/" > .gitignore
 git add .
 git commit -m 'blog'
 git push origin master
-mkdocs gh-deploy
+python -m mkdocs gh-deploy
